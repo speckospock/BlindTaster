341156d5c77689b93529ed8e60dc07241602372a
Benjamin Stephens <bs@NCC1776.local>
2017-09-25 16:01:37 -0700

    implements webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'client/public');
+const APP_DIR = path.resolve(__dirname, 'client/src');
+
+const config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  }
+};
+
+module.exports = config;
