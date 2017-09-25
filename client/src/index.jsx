d2444f18169d6a339120938ff8ab6c619a3e1d2e
Benjamin Stephens <bs@NCC1776.local>
2017-09-25 16:21:13 -0700

    sets up react

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,10 @@
 import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return <h3>HIIIIIIII</h3>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
