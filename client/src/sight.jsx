4775ba79dc5f03dacc659881e024968daa687fce
Benjamin Stephens <bs@NCC1776.local>
2017-09-26 08:59:06 -0700

    implements sight dropdowns

--- client/src/sight.jsx
+++ client/src/sight.jsx
@@ -5,13 +5,19 @@ let Sight = (props) => (
   <div className="container">
     <div className="row">
       <form>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="clarity"
-          value={props.form.clarity}
-          >
-        </input>
+      {Object.keys(props.options).map(option => (
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {option}
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            {props.options[option].map(el => {
+              console.log(el);
+              return <button className='dropdown-item' type="button">{el}</button>;
+            })}
+          </div>
+        </div>
+      ))}
       </form>
       {console.log(props.options)}
     </div>
