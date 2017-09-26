54121f8b12351f13f88d0464d21ecc3c92415660
Benjamin Stephens <bs@NCC1776.local>
2017-09-26 15:15:44 -0700

    url parsing

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,17 @@ import Test from './testBuilder.jsx';
 import Grid from './grid.jsx';
 
 class App extends React.Component {
+  componentWillMount() {
+    if (window.location.href.split('?').length > 1) {
+      let stuff = {}
+      window.location.href.split('?')[1].split('&').map(el => {
+        let things = el.split('=');
+        stuff[things[0]] = things[1];
+      });
+      console.log("cccc", stuff);
+    }
+    console.log("bbbb", window.location.href.split('?')[1].split('&'));
+  }
   render() {
     return (
       <div className="container">
